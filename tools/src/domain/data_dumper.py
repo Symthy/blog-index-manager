500638faabfdeddf7f897914c0fbd40e8b6cab83
Satoshi Yamashita <sympathia3104@gmail.com>
2022-01-09 01:54:49 +0900

    chore: change branch name and tool name

--- tools/src/domain/data_dumper.py
+++ tools/src/domain/data_dumper.py
@@ -5,7 +5,6 @@ from file.file_accessor import load_json, dump_json
 from file.files_operator import is_exist_file
 
 
-# Todo: need?
 def resolve_dump_field_data(entry, dump_data, field_name: Union[str, List[str]]) -> str:
     new_field_data = getattr(entry, field_name)
     if dump_data is None:
@@ -15,7 +14,6 @@ def resolve_dump_field_data(entry, dump_data, field_name: Union[str, List[str]])
     return getattr(entry, field_name)
 
 
-# Todo: need?
 def dump_entry_data(builder: IDumpDataBuilder, dump_file_path: str):
     exist_file_data = None
     if is_exist_file(dump_file_path):
