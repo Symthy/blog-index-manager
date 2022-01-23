f8f2acf964784b35c96cb09640b3683be9ad4fe0
Satoshi Yamashita <sympathia3104@gmail.com>
2022-01-23 12:35:53 +0900

    refactor: doc initializer

--- tools/src/service/local/docs_initializer.py
+++ tools/src/service/local/docs_initializer.py
@@ -1,7 +1,6 @@
-import os
-
 from common.constant import DOCS_DIR_PATH
 from files.conf.category_group_def import CategoryGroupDef
+from files.files_operator import get_files, make_new_dir, is_exist_dir, delete_dir
 
 
 def initialize_docs_dir(category_group_def: CategoryGroupDef):
@@ -10,15 +9,16 @@ def initialize_docs_dir(category_group_def: CategoryGroupDef):
     :param category_group_def:
     :return:
     """
-    if not os.path.exists(DOCS_DIR_PATH):
-        os.mkdir(DOCS_DIR_PATH)
+    if not is_exist_dir(DOCS_DIR_PATH):
+        make_new_dir(DOCS_DIR_PATH)
     for group in category_group_def.groups:
         docs_group_dir_path = DOCS_DIR_PATH + group + '/'
-        if not os.path.exists(docs_group_dir_path):
-            os.mkdir(docs_group_dir_path)
+        if not is_exist_dir(docs_group_dir_path):
+            make_new_dir(docs_group_dir_path)
         for category in category_group_def.get_categories(group):
             docs_group_category_dir_path = docs_group_dir_path + category + '/'
-            if os.path.exists(docs_group_category_dir_path):
-                # remove empty category dir
-                if len(os.listdir(docs_group_category_dir_path)) > 0:
-                    os.remove(docs_group_category_dir_path)
+            if not is_exist_dir(docs_group_category_dir_path):
+                continue
+            # remove empty category dir
+            if len(get_files(docs_group_category_dir_path)) == 0:
+                delete_dir(docs_group_category_dir_path)
